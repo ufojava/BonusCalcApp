0fe3c54644f75ca0d2c7d88f5823f69ccc1a7f84
Ufuoma Okoro <ufookoro@gmail.com>
2019-11-11 22:53:03 +0000

    (2) Bonus calculation phase 2

--- BonusCalcApp/ContentView.swift
+++ BonusCalcApp/ContentView.swift
@@ -6,11 +6,84 @@
 //  Copyright © 2019 Ufuoma Okoro. All rights reserved.
 //
 
+/*
+ This app is going to be a Company Bonus Calculator
+ Staff Name
+ Department
+ Year of Service
+ Salary
+ */
+ 
+
 import SwiftUI
 
+
+
+
 struct ContentView: View {
+    
+    @State private var yrOfService = 0
+    @State private var staffSal = ""
+    
+    var yearsOfSrvice = [1,2,3,4,5,6,7,8,9,10]
+    
+    //Get the value of the Picker
+    var  bonusCal: Double {
+        let totalYrOfService = Double(yrOfService)
+        let totalStaffSal = Double(staffSal) ?? 0
+        
+        //Calc value
+        let totalCalc = totalYrOfService * totalStaffSal
+        
+        return totalCalc
+    }
+        
+    
+    
     var body: some View {
-        Text("Hello, World!")
+    
+        
+        NavigationView {
+        
+        Form {
+            
+            Section { //Section 1
+                
+                Picker("Enter Year of Service",selection: $yrOfService) {
+                       ForEach( 0 ..< yearsOfSrvice.count) { yrOfSrv in
+                        
+                           
+                           Text("This is the number \(yrOfSrv)")
+                        
+                           
+                       }
+
+                   }
+            
+            }
+            
+            Section { //Section 2
+                
+                TextField("Enter Your salary",text: $staffSal)
+            
+                Text("Year of Service: \(yrOfService)")
+                Text("Total bonus calc: £\(bonusCal, specifier: "%.2f")")
+                
+            }
+            
+            
+            
+            
+            
+            
+            
+        }
+        .navigationBarTitle("Bonus Calc")
+        }
+       
+
+        
+        
     }
 }
 
