58d087667d7b9338120dfb8fad30127c03e87492
Ufuoma Okoro <ufookoro@gmail.com>
2019-11-12 15:47:06 +0000

    (1) Completed Mini Bonus Calculator Project

--- BonusCalcApp/ContentView.swift
+++ BonusCalcApp/ContentView.swift
@@ -24,18 +24,47 @@ struct ContentView: View {
     
     @State private var yrOfService = 0
     @State private var staffSal = ""
+    @State private var staffDept = 0
     
-    var yearsOfSrvice = [1,2,3,4,5,6,7,8,9,10]
+    let inStaffDept = ["HR", "ITG","CORP"]
+    
+    var yearsOfSrvice = [1,2,3,4,5,6,7,8,9,10,11]
     
     //Get the value of the Picker
     var  bonusCal: Double {
         let totalYrOfService = Double(yrOfService)
         let totalStaffSal = Double(staffSal) ?? 0
+        var bonusShare = 0.0
         
         //Calc value
-        let totalCalc = totalYrOfService * totalStaffSal
+        let yrSalaryAllocate = totalStaffSal / 12
+    
         
-        return totalCalc
+    
+        
+        if (inStaffDept[0] == "HR") && (totalYrOfService > 0 && totalYrOfService < 6) {
+            
+            bonusShare = 500.00
+            
+            return bonusShare + yrSalaryAllocate
+            
+            
+        } else if (inStaffDept[1] == "ITG") && (totalYrOfService >= 6 && totalYrOfService <= 9){
+            
+            bonusShare = 750.00
+            
+            return bonusShare + yrSalaryAllocate
+            
+        } else if (inStaffDept[2] == "CORP") && (totalYrOfService > 9) {
+        
+            bonusShare = 1000.00
+            
+            return bonusShare + yrSalaryAllocate
+    }
+    
+    
+        //return yrSalaryAllocate
+       return bonusShare
     }
         
     
@@ -47,13 +76,20 @@ struct ContentView: View {
         
         Form {
             
-            Section { //Section 1
+     
+            
+            
+            Section { //Section 2
                 
                 Picker("Enter Year of Service",selection: $yrOfService) {
                        ForEach( 0 ..< yearsOfSrvice.count) { yrOfSrv in
                         
+                        VStack {
+                            Text("\(yrOfSrv) Years")
+                        
+                        }
+                    
                            
-                           Text("This is the number \(yrOfSrv)")
                         
                            
                        }
@@ -62,7 +98,36 @@ struct ContentView: View {
             
             }
             
-            Section { //Section 2
+            
+            Section(header: Text("Select your department")) { //Section 1
+                     
+                     Picker("Department", selection: $staffDept) {
+                        
+                         ForEach(0 ..< inStaffDept.count) {
+                             
+                             
+                                Text("\(self.inStaffDept[$0])")
+                             
+                             
+                             
+                         }
+                     }
+                     .pickerStyle(SegmentedPickerStyle())
+                     
+                     
+                 }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            Section { //Section 3
+                
                 
                 TextField("Enter Your salary",text: $staffSal)
             
